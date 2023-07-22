585a472cdcb922894a3c52a68a3fd8480625ae41
IMRANDIL <aliimranadil2@gmail.com>
2023-07-22 18:38:11 +0530

    streaming app to the core

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { toast } from "react-toastify";
+import ReactPlayer from "react-player";
 import "./App.css";
 
 export default function App() {
@@ -8,10 +9,9 @@ export default function App() {
   const [videoID, setVideoID] = useState<string | null>(null);
   const [videos, setVideos] = useState<Video[]>([]);
   const [loading, setLoading] = useState(false); // State for the loading spinner
-  const currentPlayingVideoRef = useRef<string | null>(null);
-  const lastPlayedVideoRef = useRef<HTMLVideoElement | null>(null);
-  const lastPlayedTimeRef = useRef<number>(0);
-  const userSeeking = useRef<boolean>(false);
+  const [currentPlayingVideo, setCurrentPlayingVideo] = useState<string | null>(
+    null
+  );
 
   const isVideoFileType = (filename: string) => {
     // Add more video formats here if needed
@@ -23,7 +23,7 @@ export default function App() {
   const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.files && e.target.files.length > 0) {
       const file = e.target.files[0];
-      if (file.size <= 10 * 1024 * 1024) {
+      if (file.size <= 200 * 1024 * 1024) {
         // Check if the file size is within the allowed limit (10MB)
         if (isVideoFileType(file.name)) {
           setSelectedFile(file);
@@ -75,52 +75,28 @@ export default function App() {
     }
   };
 
-  const handleVideoPlay = (videoID: string) => {
-    if (!userSeeking.current) {
-      // Pause the previous video if it exists
-      if (
-        lastPlayedVideoRef.current &&
-        lastPlayedVideoRef.current !== videoID
-      ) {
-        lastPlayedVideoRef.current.pause();
-      }
-
-      // Update the lastPlayedVideoRef with the currently playing video element
-      lastPlayedVideoRef.current = document.getElementById(
-        videoID
-      ) as HTMLVideoElement;
-
-      if (currentPlayingVideoRef.current === videoID) {
-        // If it's the same video that was paused, seek to the last paused position
-        lastPlayedVideoRef.current.currentTime = lastPlayedTimeRef.current;
-      }
-
-      // Update the current playing video and reset the last paused time
-      currentPlayingVideoRef.current = videoID;
-      lastPlayedTimeRef.current = 0;
-      userSeeking.current = false;
-    }
-  };
-
-  const handleVideoPause = () => {
-    if (currentPlayingVideoRef.current) {
-      // Store the current playback position when pausing the video
-      lastPlayedTimeRef.current = lastPlayedVideoRef.current?.currentTime || 0;
-    }
-    currentPlayingVideoRef.current = null;
-  };
-
-  const handleSeeking = () => {
-    userSeeking.current = true;
-  };
-
   useEffect(() => {
     // Fetch the initial list of videos on component mount
     fetchVideos();
   }, []);
 
+  const handleVideoPlay = (videoID: string) => {
+    // If the clicked video is the same as the currently playing video, pause it
+    if (currentPlayingVideo === videoID) {
+      setCurrentPlayingVideo(null);
+    } else {
+      // If a different video is clicked, set the video ID and pause the currently playing video (if any)
+      setVideoID(videoID);
+      setCurrentPlayingVideo(videoID);
+    }
+  };
+
+  const handleVideoPause = () => {
+    setCurrentPlayingVideo(null);
+  };
+
   return (
-    <div>
+    <div className="grandParent">
       <div className="upload">
         {loading ? (
           // Show the loading spinner if the file is being uploaded
@@ -136,31 +112,27 @@ export default function App() {
         <input type="file" id="fileInput" onChange={handleFileChange} />
         <button onClick={handleUpload}>Upload</button>
       </div>
+      <h2 style={{ textAlign: "center" }}>All Videos</h2>
 
       <div className="video-list">
         {videos.length > 0 ? (
-          <>
-            <h2>All Videos</h2>
+          <div className="video-list-inner">
             {videos.map((video) => (
               <div key={video.id} className="video-card">
                 <p>Video ID: {video.id}</p>
                 <p>Video Name: {video.name}</p>
-                <video
-                  key={video.id} // Add key attribute to the video element
-                  id={video.id}
+                <ReactPlayer
+                  url={video.url}
                   width="320"
                   height="240"
                   controls
+                  playing={currentPlayingVideo === video.id}
                   onPlay={() => handleVideoPlay(video.id)}
                   onPause={handleVideoPause}
-                  onSeeking={handleSeeking}
-                >
-                  <source src={video.url} type="video/mp4" />
-                  Your browser does not support the video tag.
-                </video>
+                />
               </div>
             ))}
-          </>
+          </div>
         ) : (
           <div
             style={{
